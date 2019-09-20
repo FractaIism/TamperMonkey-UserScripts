01c812b9891f7af9e04d564a67c37427c3b4238f
FractaIism <48597961+FractaIism@users.noreply.github.com>
2019-09-20 23:12:53 +0800

    update description

--- RainbowSelection.js
+++ RainbowSelection.js
@@ -24,7 +24,7 @@
 // ==UserScript==
 // @name        Rainbow Selection
 // @author      Anveshak, Fractalism
-// @description Adapted from Shadow Selection by Anveshak. Create dynamic text shadows for highlighted text using built-in algorithms or perhaps write your very own!
+// @description Adapted from Shadow Selection by Anveshak. Create custom dynamic text shadows for highlighted text.
 // @namespace   Fractalism
 // @include     *
 // @version     0.0.2
